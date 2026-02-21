3ce1dc8f620c35fdb256bcd3122d66959420a947
soymarceloooo <soymarceloooo@gmail.com>
2026-02-21 09:50:03 -0600

    fix: revert to Next 16 + React 19, disable mapa temporarily
    - Revert package.json to original versions (Next 16.1.6 + React 19)
    - Clean next.config.ts (remove experimental turbo config)
    - Disable mapa-parques page temporarily (renamed to _mapa-parques-disabled)
    - Site builds successfully now
    
    Mapa will be re-implemented with alternative solution (Leaflet or custom integration)

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  transpilePackages: ['react-map-gl', 'mapbox-gl'],
-  experimental: {
-    turbo: {
-      resolveAlias: {
-        'react-map-gl': './node_modules/react-map-gl/dist/esm/index.js',
-      },
-    },
-  },
+  /* config options here */
 };
 
 export default nextConfig;
